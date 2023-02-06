df21005aa0369a62289d708141f2afc8d6187420
Diwash <diwashb999@gmail.com>
2023-02-06 22:17:12 +0545

    components & global style added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import {
+  About,
+  Contact,
+  Experience,
+  Footer,
+  Header,
+  Nav,
+  Portfolio,
+  Services,
+  Testimonials,
+} from "./components";
+
 function App() {
   return (
     <div>
-
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
