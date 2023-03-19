f8a990fd599a6abae526ffa7ff964f86da42e49f
Diwash <diwashb999@gmail.com>
2023-03-19 21:56:42 +0545

    New project added

--- src/components/data/Project.js
+++ src/components/data/Project.js
@@ -2,12 +2,20 @@ import img1 from "../../assets/quizApp-project.png";
 import img2 from "../../assets/memeGen-project.png";
 import img3 from "../../assets/tenzie-project.png";
 import img4 from "../../assets/javaScriptMini-project.png";
-import img5 from "../../assets/5.jpg";
-import img6 from "../../assets/6.jpg";
+import img5 from "../../assets/movie-app-project.png";
 
 const Projects = [
   {
-    id: 1,
+    id: 5,
+    name: "Movie App-React|Redux",
+    description:
+      "Moviz App provides the information of all the movies including trending, popular, upcoming and many more. You can find any movies in this app. This app uses the TMDB api to fetch the data of moives.",
+    img: img5,
+    github: "https://github.com/diwashbhattarai999/movie_app_react",
+    link: "https://db-movie-app.vercel.app/",
+  },
+  {
+    id: 4,
     name: "Quiz App-React",
     description:
       "Quiz App is a general Knowledge and IQ game with a variety of options to play. There are different levels from easy to hard and different categories to choose for a player.",
@@ -16,7 +24,7 @@ const Projects = [
     link: "https://quiz-app-diwash.netlify.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "Meme Generator-React",
     description:
       "Meme Generator is a random meme generator with a text on that particular image. The user can generate random popular meme image and can write their own text to make their own custom meme.",
@@ -25,7 +33,7 @@ const Projects = [
     link: "https://github.com/diwashbhattarai999/meme_generator_react",
   },
   {
-    id: 3,
+    id: 2,
     name: "Tenzie Game-React",
     description:
       "Tenzie is a board game with random numbers on the board and a dice. A dice is rolled and same numbers are choosen again and again until all the numbers are same.",
@@ -34,29 +42,13 @@ const Projects = [
     link: "https://github.com/diwashbhattarai999/Tenzie_game_react",
   },
   {
-    id: 4,
+    id: 1,
     name: "Mini Project-JavaScript",
     description:
       "Some small projects like Random joke generator, Google keep clone, turn light off/on and many other mini projects made using JavaScript. Check it out at github.",
     img: img4,
     github: "https://github.com/diwashbhattarai999/Js-mini-projects",
     link: "https://github.com/diwashbhattarai999/Js-mini-projects",
-  },
-  {
-    id: 5,
-    name: "Project 5",
-    description: "Adding when its ready Note: The image is just dummy image",
-    img: img5,
-    github: "github.com",
-    link: "github.com",
-  },
-  {
-    id: 6,
-    name: "Project 6",
-    description: "Adding when its ready!! Note: The image is just dummy image",
-    img: img6,
-    github: "github.com",
-    link: "github.com",
   },
 ];
 
