2e9f8d7567faf3bc72e8f6db2452cc8903f67a47
Diwash <diwashb999@gmail.com>
2023-03-20 19:13:46 +0545

    Project Updated

--- src/components/data/Project.js
+++ src/components/data/Project.js
@@ -3,10 +3,11 @@ import img2 from "../../assets/memeGen-project.png";
 import img3 from "../../assets/tenzie-project.png";
 import img4 from "../../assets/javaScriptMini-project.png";
 import img5 from "../../assets/movie-app-project.png";
+import img6 from "../../assets/social-media-js-project.png";
 
 const Projects = [
   {
-    id: 5,
+    id: 6,
     name: "Movie App-React|Redux",
     description:
       "Moviz App provides the information of all the movies including trending, popular, upcoming and many more. You can find any movies in this app. This app uses the TMDB api to fetch the data of moives.",
@@ -15,13 +16,22 @@ const Projects = [
     link: "https://db-movie-app.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     name: "Quiz App-React",
     description:
       "Quiz App is a general Knowledge and IQ game with a variety of options to play. There are different levels from easy to hard and different categories to choose for a player.",
     img: img1,
     github: "https://github.com/diwashbhattarai999/quiz_app",
     link: "https://quiz-app-diwash.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Social Media-JS",
+    description:
+      "This social media app is just a ui of social media chatting app made with HTML and CSS.Also,the functionality like theme customization, notification popup are added with JavaScript.",
+    img: img6,
+    github: "https://github.com/diwashbhattarai999/social_media_js",
+    link: "https://diwashbhattarai999.github.io/social_media_js/",
   },
   {
     id: 3,
