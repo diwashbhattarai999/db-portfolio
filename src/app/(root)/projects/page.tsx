4bfc20c7afdcb7009b3e19ae8f7e964c414fbeb7
Diwash Bhattarai <diwashb999@gmail.com>
2024-03-02 09:51:52 +0545

    feat: add metadata in about & project page

--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -1,4 +1,11 @@
 import Projects from "@/components/sections/Projects";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Projects | Diwash Bhattarai",
+  description:
+    "Explore my portfolio of projects, showcasing diverse skills and experiences.",
+};
 
 const ProjectPage = () => {
   return <Projects />;
